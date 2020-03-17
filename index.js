817d1c43cdd8a75b4358d30441eafcc2f255241c
Amy Franz <amy.franz@gmail.com>
2020-03-17 00:21:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Your code here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = (activity = "go to the office") => {
+  return `This Monday, I will ${activity}.`;
+};
+
+const wrapAdjective = (wrap = "*") => {
+  debugger;
+  if (wrap == "*" || wrap == "||") {
+    return function(adject) {
+      return `You are ${wrap}${adject}${wrap}!`;
+    };
+  }
+};
+
+const Calculator = {
+  add: function(num1, num2) {
+    return num1 + num2;
+  },
+  subtract: function(num1, num2) {
+    return num1 - num2;
+  },
+  multiply: function(num1, num2) {
+    return num1 * num2;
+  },
+  divide: function(num1, num2) {
+    return num1 / num2;
+  }
+};
+
+const actionApplyer = (int, arr) => {
+  if (arr.length == 0) {
+    return int;
+  }
+  let base = int;
+  for (let i = 0; i < arr.length; i++) {
+    base = arr[i](base);
+  }
+  return base;
+};
